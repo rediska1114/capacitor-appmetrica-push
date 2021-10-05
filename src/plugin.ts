d8069d47ff2d67f7c19cedffa65bb38b282d0e0c
Stepan Kotov <rediska1114@gmail.com>
2021-10-05 20:10:46 +0300

    fix: fix build

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,8 +1,8 @@
-import { AppmetricaPushPlugin as IAppmetricaPushPlugin } from './definitions';
-import { Plugins } from '@capacitor/core';
+// import { AppmetricaPushPlugin as IAppmetricaPushPlugin } from './definitions';
+// import { Plugins } from '@capacitor/core';
 
-const AppmetricaPushPlugin = Plugins.Appmetrica as IAppmetricaPushPlugin;
+// const AppmetricaPushPlugin = Plugins.Appmetrica as IAppmetricaPushPlugin;
 
 export class AppmetricaPush {
-  private appmetricaPush = AppmetricaPushPlugin;
+  // private appmetricaPush = AppmetricaPushPlugin;
 }
